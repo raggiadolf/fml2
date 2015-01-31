62c014cf0c7486e90415374e4612e64c495ae62e
Ragnar Borgþór Ragnarsson <ragnarbr1992@gmail.com>
2015-01-31 20:12:33 +0000

    Parser.. implementation..

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.io.Reader;
+import java.io.InputStreamReader;
 /**
  * Should contain a public method, nextToken(), which scans the standard input (stdin),
  * looking for patterns that match one of the tokens from Token.
@@ -11,11 +14,77 @@
  * The lexical analyzer returns a token with TokenCode = ERROR if some illegal lexeme is found.
  */
 public class Lexer {
+    private int nextTokenIndex = 0;
+    private ArrayList<Token> tokens = null;
+    private Reader reader = new InputStreamReader(System.in);
+    private char nextChar;
 
-    public void nextToken() {
-        /*
-         *  Scans the standard input(stdin),
-         *  looking for patterns that match one of the tokens from Token?
-         */
+    /*
+   *  Scans the standard input(stdin),
+   *  looking for patterns that match one of the tokens from Token?
+   */
+    public TokenCode nextToken() {
+        String curLexeme = "";
+
+        try {
+            int ch;
+            while ((ch = reader.read()) != -1) {
+                nextChar = Character.toChars(ch)[0];
+                curLexeme += c;
+                System.out.println(c);
+            }
+        }
+        catch (java.io.IOException ex) {
+
+        }
+
+        return lookup(curLexeme);
+    }
+
+    private char getChar() {
+        try {
+            int ch = reader.read();
+
+
+        }
+
+        catch (java.io.IOException ex) {
+        }
+    }
+
+    //Returns null if Lexeme is not reserved, otherwise, the tokencode for the lexeme
+    private TokenCode lookup(String lexeme) {
+        TokenCode retValue = null;
+        if (lexeme == "+") {
+            retValue = TokenCode.PLUS;
+        }
+        else if (lexeme == "-") {
+            retValue = TokenCode.MINUS;
+        }
+        else if (lexeme == "*") {
+            retValue = TokenCode.MULT;
+        }
+        else if (lexeme == "(") {
+            retValue = TokenCode.LPAREN;
+        }
+        else if (lexeme == ")") {
+            retValue = TokenCode.RPAREN;
+        }
+        else if (lexeme == ";") {
+            retValue = TokenCode.SEMICOL;
+        }
+        else if (lexeme == "end") {
+            retValue = TokenCode.END;
+        }
+        else if (lexeme == "print") {
+            retValue = TokenCode.PRINT;
+        }
+
+        return retValue;
+    }
+
+    public static void main(String[] args) {
+        Lexer lex = new Lexer();
+        lex.nextToken();
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -10,6 +10,112 @@
  * Lexer, the other of type Token (for the current token). It should only have a
  * single public method, parse(), for initiating the parse - other methods are private.
  */
-public class Parser {
 
+/*
+   Grammar G for language L
+   Statements -> Statement ; Statements | end
+   Statement -> id = Expr | print id
+   Expr -> Term | Term + Expr | Term - Expr
+   Term -> Factor | Factor * Term
+   Factor -> int | id | (Expr)
+*/
+ */
+public class Parser {
+    private Lexer lex = new Lexer(), Token token;
+    public void parse() {
+        token = lex.nextToken();
+        Statements();
+    }
+
+    private void Statements() {
+        if (token.tCode == TokenCode.END) {
+            token = lex.nextToken();
+        }
+        else {
+            Statement();
+            if (token.tCode == TokenCode.SEMICOL)
+                token = lex.nextToken();
+            else
+                Error();
+            Statements();
+        }
+    }
+
+    private void Statement() {
+        if (token.tCode == TokenCode.ID) {
+            token = lex.nextToken();
+            if (token.tCode == TokenCode.ASSIGN) {
+                token = lex.nextToken();
+                Expr();
+            }
+            else {
+                error();
+            }
+        }
+        else if (token.tCode == TokenCode.PRINT) {
+            token = lex.nextToken();
+            if (token.tCode == TokenCode.id) {
+                token = lex.nextToken();
+            }
+            else {
+                error();
+            }
+        }
+        else {
+            error();
+        }
+    }
+
+    private void Expr() {
+        Term();
+
+        if (token.tCode == TokenCode.PLUS) {
+            token = lex.nextToken();
+            Expr();
+        }
+        else if (token.tCode == TokenCode.MINUS) {
+            token = lex.nextToken();
+            Expr();
+        }
+        //Engin error.. má vera bara term og ekkert meira..
+    }
+
+    private void Term() {
+        Factor();
+
+        if (token.tCode == TokenCode.MULT) {
+            token = lex.nextToken();
+            Term();
+        }
+
+        //Ekkert error.. má vera bara factor..
+    }
+
+    private void Factor() {
+        if (token.tCode == TokenCode.INT) {
+            token = lex.nextToken();
+        }
+        else if (token.tCode == TokenCode.ID) {
+            token = lex.nextToken();
+        }
+        else if (token.tCode == TokenCode.LPAREN) {
+            token = lex.nextToken();
+            Expr();
+            if (token.tCode == TokenCode.RPAREN) {
+                token = lex.nextToken();
+            }
+            else {
+                error();
+            }
+        }
+        //Fengum ekkert af þessu? Ekki valid Factor..
+        else {
+            error();
+        }
+    }
+
+    private void Error() {
+        System.out.println("Syntax error!");
+        System.exit(0);
+    }
 }
